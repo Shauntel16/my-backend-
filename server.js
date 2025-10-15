55cfa0470e11547d8f478a7755bf08735fa525a1
trypho <kgobemadimetja257@gmail.com>
2025-10-15 11:56:11 +0200

    Added the report disruption APIs

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const addStudRoute = require("./src/api/v1/routes/addStudRoute");
-const requestLeaveRoute = require("./src/api/v1/routes/requestLeaveRoute");
 const bodyParser = require("body-parser");
 
+const addStudRoute = require("./src/api/v1/routes/addStudRoute");
+const requestLeaveRoute = require("./src/api/v1/routes/requestLeaveRoute");
+const reportClosureRoute = require("./src/api/v1/routes/reportClosureRoute");
+
 const app = express();
+
 app.use(bodyParser.json());
 
 app.use(addStudRoute);
 app.use(requestLeaveRoute);
+app.use(reportClosureRoute);
 
 app.get("/", (req, res, next) => {
   res.send("Home page reached");
